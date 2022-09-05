41a19ef079d3294081968899d1c022644b2a5b48
SeregeKEK <kurnakov2012@mail.ru>
2022-09-05 16:21:10 +0300

    добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Zadacha(){
+    string[] array = new string[]{"wasd", "qwerty", "boi", "12","╚╦╩"};
+    Console.WriteLine("Изначальный массив");
+    PrintArray(array);
+    string sort = string.Empty;
+    for( int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            sort += array[i] + ", ";
+        }
+    }
+    
+    sort = sort.Remove(sort.Length - 2);
+    string[] array2 = new string[]{sort};
+    Console.WriteLine("Отсортированный массив");
+    PrintArray(array2);
+}
+
+void PrintArray(string[] array){
+    for( int i = 0; i < array.Length; i++){
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write("\b\b.");
+    Console.WriteLine();
+}
+
+Zadacha();
